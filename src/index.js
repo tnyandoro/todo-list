7e1105665f7640e6980cb38c91d5c4fd9c11bdff
Tendai Nyandoro <tendai1@hotmail.com>
2021-05-12 16:47:11 +0200

    Set the transpilers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import _ from 'lodash';
+import './style.css';
+import Bg from './images/wood-bg.jpg';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myBg = new Image();
+  myBg.src = Bg;
+
+  element.appendChild(myBg);
 
   return element;
 }
