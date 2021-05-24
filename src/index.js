9be008ce5d593352b92110bf61b59d8507891763
Tendai Nyandoro <tendai1@hotmail.com>
2021-05-24 11:47:07 +0200

    Set task form

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import { divide } from 'lodash';
 // import { format, compareAsc } from 'date-fns';
-import { person, sayHello } from './todo';
-
-const datefns = require('date-fns');
-
-const arrDates = [new Date(1985, 5, 30), new Date(2007, 11, 15), new Date()];
-
-console.log(arrDates);
-
-arrDates.forEach((date) => {
-  console.log(datefns.addYears(date, 5));
-});
+import { todoTask } from './todo';
+import Project from './projects';
+ 
